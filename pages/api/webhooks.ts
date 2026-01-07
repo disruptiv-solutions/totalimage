82045b2558cc2bfb1afab6f91dd675e8015c7f30
disruptiv-solutions <info@disruptiv.solutions>
2026-01-07 17:48:39 -0600

    Update Stripe API version to 2025-02-24.acacia - Updated all Stripe API versions from 2025-01-27.acacia to 2025-02-24.acacia - Fixed TypeScript compatibility error - Updated in: create-checkout-session, webhooks, get-subscription, get-customer, create-customer

--- pages/api/webhooks.ts
+++ pages/api/webhooks.ts
@@ -4,7 +4,7 @@ import Stripe from 'stripe';
 import { adminDb } from '../../lib/firebase-admin';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2025-01-27.acacia',
+  apiVersion: '2025-02-24.acacia',
 });
 
 export const config = {
