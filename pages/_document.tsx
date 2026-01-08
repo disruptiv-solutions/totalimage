0c6e9c56974422232347a9134d291caf0d20e010
disruptiv-solutions <info@disruptiv.solutions>
2026-01-07 18:53:12 -0600

    Add favicon support with all icon sizes and web manifest - Added favicon.ico, favicon-16x16.png, favicon-32x32.png - Added Apple Touch Icon (apple-touch-icon.png) - Added Android Chrome icons (192x192 and 512x512) - Added site.webmanifest for PWA support - Updated _document.tsx with all favicon links

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,24 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0, user-scalable=yes" />
+        
+        {/* Favicon */}
+        <link rel="icon" type="image/x-icon" href="/favicon/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+        
+        {/* Apple Touch Icon */}
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+        
+        {/* Android Chrome Icons */}
+        <link rel="icon" type="image/png" sizes="192x192" href="/favicon/android-chrome-192x192.png" />
+        <link rel="icon" type="image/png" sizes="512x512" href="/favicon/android-chrome-512x512.png" />
+        
+        {/* Web Manifest */}
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+        
+        {/* Theme Color */}
+        <meta name="theme-color" content="#000000" />
       </Head>
       <body>
         <Main />
